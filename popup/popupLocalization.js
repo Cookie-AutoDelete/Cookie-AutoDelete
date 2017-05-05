72438bd537eec73189401b4bb39e299f4a155fde
mrdokenny <mrdokenny@gmail.com>
2017-05-04 22:24:23 -0700

    Fixed CSS issues with Localization in Popup

--- popup/popupLocalization.js
+++ popup/popupLocalization.js
@@ -1,6 +1,6 @@
 document.getElementById("hostWebsiteText").textContent = browser.i18n.getMessage("hostWebsiteText");
-document.getElementById("cookieCleanup").textContent = browser.i18n.getMessage("cookieCleanupText");
-document.getElementById("cookieCleanupIgnoreOpenTabs").textContent = browser.i18n.getMessage("cookieCleanupIgnoreOpenTabsText");
-document.getElementById("clearCookiesForDomain").textContent = browser.i18n.getMessage("clearCookiesForDomainText");
+document.getElementById("cookieCleanupText").textContent = browser.i18n.getMessage("cookieCleanupText");
+document.getElementById("cookieCleanupIgnoreOpenTabsText").textContent = browser.i18n.getMessage("cookieCleanupIgnoreOpenTabsText");
+document.getElementById("clearCookiesForDomainText").textContent = browser.i18n.getMessage("clearCookiesForDomainText");
 document.getElementById("activeModeSwitchText").textContent = browser.i18n.getMessage("activeModeSwitchText");
 document.getElementById("switchToWhiteListText").textContent = browser.i18n.getMessage("switchToWhiteListText");
