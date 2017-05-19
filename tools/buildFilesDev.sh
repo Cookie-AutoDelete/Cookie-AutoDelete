598dd64583a95b5c7581bab5af6d092277fe0a45
mrdokenny <mrdokenny@gmail.com>
2017-05-19 12:45:22 -0700

    Updated Build Tools to add Extension Name

--- tools/buildFilesDev.sh
+++ tools/buildFilesDev.sh
@@ -1,16 +1,17 @@
 echo "Building"
 
+EXTENSIONNAME="Cookie-AutoDelete"
 DES=builds
 DATETIME=$(date +"%m%d%y"_%H%M)
-FIREFOXFILENAME=Firefox_Dev
-CHROMEFILENAME=Chrome_Dev
+FIREFOXFILENAME=${EXTENSIONNAME}_Firefox_Dev_${DATETIME}
+CHROMEFILENAME=${EXTENSIONNAME}_Chrome_Dev_${DATETIME}
 
 rmdir -rf $DES
 mkdir -p $DES
 cd src/
 
-zip -r ${FIREFOXFILENAME}.zip *
-mv ${FIREFOXFILENAME}.zip ../$DES/
+zip -r ${FIREFOXFILENAME}.xpi *
+mv ${FIREFOXFILENAME}.xpi ../$DES/
 
 sed -i '/contextualIdentities/d' manifest.json
 sed -i '/applications/,+5d' manifest.json
