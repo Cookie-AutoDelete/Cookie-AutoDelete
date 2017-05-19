591f59c0d6cac6405ec5eba986dfab386c5fb5e3
mrdokenny <mrdokenny@gmail.com>
2017-05-19 08:59:25 -0700

    Tweaked Build Tools

--- tools/buildFiles.sh
+++ tools/buildFiles.sh
@@ -6,14 +6,14 @@ mkdir -p $DES
 cd src/
 
 zip -r Firefox.zip *
+mv Firefox.zip ../$DES/
 
-
-sed -i '/contextualIdentities/' manifest.json
+sed -i '/contextualIdentities/d' manifest.json
 sed -i '/applications/,+5d' manifest.json
 zip -r Chrome.zip *
 
 
-mv Firefox.zip ../$DES/
+
 mv Chrome.zip ../$DES/
 
 git checkout manifest.json
