3ed8cf91a4c467cc541cdf9197c06a340733b18d
mrdokenny <mrdokenny@gmail.com>
2017-05-29 21:46:19 -0700

    100% coverage for WhiteListService

--- test/WhiteListService-spec.js
+++ test/WhiteListService-spec.js
@@ -44,6 +44,14 @@ describe("WhiteListService with Contextual Identities Off", () => {
 		assert.isTrue(browser.storage.local.set.calledOnce);
 	});
 
+	describe("constructor with no defaultWhiteList", () => {
+		let items = {};
+		let whiteListService = new WhiteListService(items);
+		it("should return an empty list for defaultWhiteList", () => {
+			assert.sameMembers(whiteListService.returnList("defaultWhiteList"), []);
+		});
+	});
+
 	afterEach(() => {
         browser.flush();
     });
@@ -60,13 +68,33 @@ describe("WhiteListService with Contextual Identities On", () => {
 
 	cache.nameCacheMap.set("firefox_container_1", "Personal");
 	cache.nameCacheMap.set("firefox_container_2", "Work");
-	
+	cache.nameCacheMap.set("firefox_container_5", "Play");
+
 	global.browser = browser;
 
 	beforeEach(() => {
 		whiteListService = new WhiteListService(items, true, cache);
 	});
+	
+	describe("constructor with firefox-default in items", () => {
+		var firefoxDefault = "firefox-default";
+		let items = [];
+		items[firefoxDefault] = ["youtube.com", "google.com", "facebook.com"];
+		let whiteListService;
+		beforeEach(() => {
+			whiteListService = new WhiteListService(items, true, cache);
+		});
+		
+		it("should return [youtube.com, google.com, facebook.com]} for firefox-default", () => {
+			assert.sameMembers(whiteListService.returnList(firefoxDefault), ["youtube.com", "google.com", "facebook.com"]);
+		});
+	});
 
+	describe("constructor with no firefox_container_5 in items", () => {
+		it("should return an empty list for firefox_container_5", () => {
+			assert.sameMembers(whiteListService.returnList("firefox_container_5"), []);
+		});
+	});
 	it("hasHost() should return true for youtube.com in Personal", () => {
 		assert.isTrue(whiteListService.hasHost("youtube.com", "firefox_container_1"));
 	});
@@ -79,6 +107,10 @@ describe("WhiteListService with Contextual Identities On", () => {
 		assert.isFalse(whiteListService.hasHost("youtube.com", "firefox_container_2"));
 	});
 
+	it("hasHost() should return false for google.com in non existant container", () => {
+		assert.isFalse(whiteListService.hasHost("google.com", "firefox_container_3"));
+	});
+
 
 	it("returns [youtube.com, google.com, facebook.com] for returnList() in Personal", () => {
 		assert.sameMembers(whiteListService.returnList("firefox_container_1"), ["youtube.com", "google.com", "facebook.com"]);
@@ -88,10 +120,20 @@ describe("WhiteListService with Contextual Identities On", () => {
 		assert.sameMembers(whiteListService.returnList("firefox_container_2"), ["github.com"]);
 	});
 
+	it("returns [] for returnList() in non existant container", () => {
+		assert.sameMembers(whiteListService.returnList("firefox_container_3"), []);
+	});
+
 	it("addURL() for yahoo.com and it should exist in Work but not Personal", () => {
 		whiteListService.addURL("yahoo.com", "firefox_container_2");
 		assert.isTrue(whiteListService.hasHost("yahoo.com", "firefox_container_2"));
 		assert.isFalse(whiteListService.hasHost("yahoo.com", "firefox_container_1"));
+		assert.isTrue(browser.storage.local.set.calledOnce);
+	});
+
+	it("addURL() for yahoo.com and it should exist in a newly created container", () => {
+		whiteListService.addURL("twitter.com", "firefox_container_3");
+		assert.isTrue(whiteListService.hasHost("twitter.com", "firefox_container_3"));
 		assert.isTrue(browser.storage.local.set.calledOnce);
 	});
 
@@ -100,6 +142,12 @@ describe("WhiteListService with Contextual Identities On", () => {
 		assert.isFalse(whiteListService.hasHost("google.com", "firefox_container_1"));
 		assert.isFalse(whiteListService.hasHost("google.com", "firefox_container_2"));
 		assert.isTrue(browser.storage.local.set.calledOnce);
+	});
+
+	it("removeURL() for twitter.com in a non existant container and it should create the container", () => {
+		whiteListService.removeURL("twitter.com", "firefox_container_3");
+		assert.isTrue(whiteListService.cookieWhiteList.has("firefox_container_3"));
+		assert.isFalse(browser.storage.local.set.calledOnce);
 	});
 
 	it("clearURL() should not have anything in the whitelist in Personal but not affect Work", () => {
