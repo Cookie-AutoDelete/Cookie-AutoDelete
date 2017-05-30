130325f48c690fe6b0e8936290a5e164d88130ef
mrdokenny <mrdokenny@gmail.com>
2017-05-29 20:44:08 -0700

    Full Code Coverage for StatsService and UsefulFunction

--- test/StatsService-spec.js
+++ test/StatsService-spec.js
@@ -24,29 +24,39 @@ describe("StatsService class with no items", () => {
 		assert.strictEqual(statsService.cookieDeletedCounter, 0);
 	});
 
-	it("returns 20 for cookieDeletedCounter for incrementCounter(20)", () => {
-		return statsService.incrementCounter(20)
-		.then(() => {
-			assert.strictEqual(statsService.cookieDeletedCounter, 20);
+	describe("incrementCounter()", () => {
+		beforeEach(() => {
+			//statsService = new StatsService(items);
+			browser.storage.local.get.resolves({statLoggingSetting: true});
 		});
-		
-	});
-	
-	it("returns 20 for cookieDeletedCounterTotal for incrementCounter(20)", () => {
-		return statsService.incrementCounter(20)
-		.then(() => {
-			assert.strictEqual(statsService.cookieDeletedCounterTotal, 20);
+
+		it("returns 20 for cookieDeletedCounter for incrementCounter(20)", () => {
+			return statsService.incrementCounter(20)
+			.then(() => {
+				assert.strictEqual(statsService.cookieDeletedCounter, 20);
+			});
 		});
+
+		it("returns 20 for cookieDeletedCounterTotal for incrementCounter(20)", () => {
+			return statsService.incrementCounter(20)
+			.then(() => {
+				assert.strictEqual(statsService.cookieDeletedCounterTotal, 20);
+			});
+				
+		});
+
+		it("calls twice on browser.storage.local.set for incrementCounter(20) (previous one was in resetCounter() at start)", () => {
+			return statsService.incrementCounter(20)
+			.then(() => {
+				assert.isTrue(browser.storage.local.set.calledTwice);
+			});
+				
+		});	
 		
 	});
 
-	it("calls twice on browser.storage.local.set for incrementCounter(20) (previous one was in resetCounter() at start)", () => {
-		return statsService.incrementCounter(20)
-		.then(() => {
-			assert.isTrue(browser.storage.local.set.calledTwice);
-		});
-		
-	});
+
+	
 	afterEach(() => {
         browser.flush();
     });
@@ -72,29 +82,37 @@ describe("StatsService class with items", () => {
 	});
 
 
-	it("returns 20 for cookieDeletedCounter for incrementCounter(20)", () => {
-		return statsService.incrementCounter(20)
-		.then(() => {
-			assert.strictEqual(statsService.cookieDeletedCounter, 20);
+	describe("incrementCounter()", () => {
+		beforeEach(() => {
+			//statsService = new StatsService(items);
+			browser.storage.local.get.resolves({statLoggingSetting: true});
 		});
-		
-	});
-	
-	it("returns 30 for cookieDeletedCounterTotal for incrementCounter(20)", () => {
-		return statsService.incrementCounter(20)
-		.then(() => {
-			assert.strictEqual(statsService.cookieDeletedCounterTotal, 30);
-		});
-		
-	});
 
-	it("calls once on browser.storage.local.set for incrementCounter(20)", () => {
-		return statsService.incrementCounter(20)
-		.then(() => {
-			assert.isTrue(browser.storage.local.set.calledOnce);
+		it("returns 20 for cookieDeletedCounter for incrementCounter(20)", () => {
+			return statsService.incrementCounter(20)
+			.then(() => {
+				assert.strictEqual(statsService.cookieDeletedCounter, 20);
+			});
+			
 		});
 		
-	});
+		it("returns 30 for cookieDeletedCounterTotal for incrementCounter(20)", () => {
+			return statsService.incrementCounter(20)
+			.then(() => {
+				assert.strictEqual(statsService.cookieDeletedCounterTotal, 30);
+			});
+			
+		});
+
+		it("calls once on browser.storage.local.set for incrementCounter(20)", () => {
+			return statsService.incrementCounter(20)
+			.then(() => {
+				assert.isTrue(browser.storage.local.set.calledOnce);
+			});
+			
+		});
+	});		
+	
 
 
 	afterEach(() => {
