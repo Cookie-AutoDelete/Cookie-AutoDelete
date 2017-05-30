72d4b1026571e05222e14d4246aabc8d121af44f
mrdokenny <mrdokenny@gmail.com>
2017-05-29 21:45:48 -0700

    100% coverage for branches on StatsService

--- test/StatsService-spec.js
+++ test/StatsService-spec.js
@@ -9,7 +9,6 @@ describe("StatsService class with no items", () => {
 
 	beforeEach(() => {
 		statsService = new StatsService(items);
-		browser.storage.local.get.resolves({statLoggingSetting: true});
 	});
 
 	it("should have been called once for resetCounter", () => {
@@ -51,8 +50,36 @@ describe("StatsService class with no items", () => {
 				assert.isTrue(browser.storage.local.set.calledTwice);
 			});
 				
-		});	
-		
+		});
+
+		describe("incrementCounter() with stat logging off", () => {
+			beforeEach(() => {
+				browser.storage.local.get.resolves({statLoggingSetting: false});
+			});
+
+			it("returns 0 for cookieDeletedCounter for incrementCounter(20)", () => {
+				return statsService.incrementCounter(20)
+				.then(() => {
+					assert.strictEqual(statsService.cookieDeletedCounter, 0);
+				});
+			});
+
+			it("returns 0 for cookieDeletedCounterTotal for incrementCounter(20)", () => {
+				return statsService.incrementCounter(20)
+				.then(() => {
+					assert.strictEqual(statsService.cookieDeletedCounterTotal, 0);
+				});
+					
+			});
+
+			it("calls once on browser.storage.local.set for incrementCounter(20) (only at start)", () => {
+				return statsService.incrementCounter(20)
+				.then(() => {
+					assert.isTrue(browser.storage.local.set.calledOnce);
+				});
+					
+			});	
+		});
 	});
 
 
