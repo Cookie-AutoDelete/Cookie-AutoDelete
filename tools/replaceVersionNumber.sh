568fc3f8ec0bae7da85b91b56512c16e985a2422
mrdokenny <mrdokenny@gmail.com>
2017-05-31 09:02:41 -0700

    Tweaked Deployment Procedures

--- tools/replaceVersionNumber.sh
+++ tools/replaceVersionNumber.sh
@@ -1,6 +1,6 @@
 echo "Replacing"
 if ! [ -z "$TRAVIS_TAG" ]; then 
- 	sed -i 's/"version.*/"version": "'"$TRAVIS_TAG"'"/' src/manifest.json
+ 	sed -i 's/"version.*/"version": "'"$TRAVIS_TAG"'"/' extension/manifest.json
 	echo "Replacement done with $TRAVIS_TAG"
 else 
 	echo "No Replacement done."
