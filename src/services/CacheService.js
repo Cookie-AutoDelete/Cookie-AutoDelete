13f2d0599bf2950c9bf66ec5ad5e44f269fafcf0
mrdokenny <mrdokenny@gmail.com>
2017-06-03 09:23:31 -0700

    Prevent returning an empty promise that leaks into whitelist

--- src/services/CacheService.js
+++ src/services/CacheService.js
@@ -8,11 +8,10 @@ class CacheService {
 	cacheContextualIdentityNames() {
 		return browser.contextualIdentities.query({})
 		.then((containers) => {
-			browser.storage.local.set({containerCache: containers});
 			containers.forEach((currentValue, index, array) => {
 				this.nameCacheMap.set(currentValue.cookieStoreId, currentValue.name);
 			});
-			return Promise.resolve();
+			return browser.storage.local.set({containerCache: containers});
 		});
 	}
 
