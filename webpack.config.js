7134d427fbb97004cccb986b5d5165389bca86d6
mrdokenny <mrdokenny@gmail.com>
2017-09-17 21:24:35 -0700

    Resolved #129

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,28 @@
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const webpack = require('webpack');
+const UglifyEsPlugin = require('uglify-es-webpack-plugin');
 const plugins = [
+  new UglifyEsPlugin({
+    output: {
+        comments: false,
+        beautify: true,
+        preserve_line: true
+    },
+    compress: false,
+    mangle: false
+  }),
+  new webpack.BannerPlugin(`
+    Copyright (c) 2017 Kenny Do
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+
+  `)
   // new BundleAnalyzerPlugin({
   //       analyzerMode: 'static'
   // })
