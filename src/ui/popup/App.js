8d3726bc9562876389e14547cebf8568152158b6
Kenny Do <contact@kennydo.com>
2017-10-07 17:00:02 -0700

    Change popup width size css to minWidth

--- src/ui/popup/App.js
+++ src/ui/popup/App.js
@@ -94,7 +94,7 @@ class App extends Component {
 						paddingBottom: "8px",
 						backgroundColor: "rgba(0, 0, 0, 0.05)",
 						borderBottom: "1px solid rgba(0, 0, 0, 0.1)",
-						width: "650px"
+						minWidth: "650px"
 					}}
 				>
 					<div className="col-xs-1">
