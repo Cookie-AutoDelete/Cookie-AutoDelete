158d04537ad2c0d1646be3337e4f7b7a92331fc9
Kenny Do <contact@kennydo.com>
2018-04-18 09:34:02 -0700

    Fix mixed tabs and spaces error

--- src/ui/common_components/ErrorBoundary.js
+++ src/ui/common_components/ErrorBoundary.js
@@ -3,49 +3,49 @@ import {connect} from "react-redux";
 import React from "react";
 
 class ErrorBoundary extends React.Component {
-    state = {
-    	hasError: false
-    };
+	state = {
+		hasError: false
+	};
 
-    componentDidCatch(error, info) {
-    	// Display fallback UI
-    	if (error !== "state is undefined") {
-    		this.setState({
-    			hasError: true,
-    			message: `
+	componentDidCatch(error, info) {
+		// Display fallback UI
+		if (error !== "state is undefined") {
+			this.setState({
+				hasError: true,
+				message: `
             ${error.message}
             ${error.stack}
             at line ${error.lineNumber}
             `
-    		});
-    	}
-    }
+			});
+		}
+	}
 
-    resetButton() {
-    	this.props.onResetButtonClick();
-    	this.setState({
-    		hasError: false
-    	});
-    }
+	resetButton() {
+		this.props.onResetButtonClick();
+		this.setState({
+			hasError: false
+		});
+	}
 
-    render() {
-    	if (this.state.hasError) {
-    		// You can render any custom fallback UI
-    		return (
-    			<div className="alert alert-danger" role="alert">
-    				<h4 className="alert-heading">Error!</h4>
-    				<p>{this.state.message}</p>
-    				<hr />
-    				<p className="mb-0">
-    					<button className="btn btn-danger" onClick={() => this.resetButton()}>
-    						<span>{browser.i18n.getMessage("defaultSettingsText")}</span>
-    					</button>
-    				</p>
-    			</div>
-    		);
-    	}
-    	return this.props.children;
-    }
+	render() {
+		if (this.state.hasError) {
+			// You can render any custom fallback UI
+			return (
+				<div className="alert alert-danger" role="alert">
+					<h4 className="alert-heading">Error!</h4>
+					<p>{this.state.message}</p>
+					<hr />
+					<p className="mb-0">
+						<button className="btn btn-danger" onClick={() => this.resetButton()}>
+							<span>{browser.i18n.getMessage("defaultSettingsText")}</span>
+						</button>
+					</p>
+				</div>
+			);
+		}
+		return this.props.children;
+	}
 }
 
 const mapDispatchToProps = (dispatch) => ({
