645863f18a1db4e6fe1351487119b3ceb1c58576
Kenny Do <contact@kennydo.com>
2018-05-05 14:47:25 -0700

    Show placeholder for the cleanup log when only private cookies are deleted

--- src/ui/common_components/ActivityTable.js
+++ src/ui/common_components/ActivityTable.js
@@ -39,62 +39,66 @@ const ActivityTable = (props) => {
 			}}
 		>
 			{
-				filtered.map((element, index) => (
-					<div key={index} className="card">
-						<div className="card-header" id={`heading${index}`}>
-							<h5 className="mb-0" style={{
-								overflowX: "hidden"
-							}}
-							>
-								<button className="btn btn-link collapsed" type="button" data-toggle="collapse" data-target={`#collapse${index}`} aria-expanded="false" aria-controls={`collapse${index}`}>
+				filtered.map((element, index) => {
+					const summary = createSummary(element);
+					const message = browser.i18n.getMessage("notificationContent", [element.recentlyCleaned, summary !== "" ? summary : "(Private)"]);
+					return (
+						<div key={index} className="card">
+							<div className="card-header" id={`heading${index}`}>
+								<h5 className="mb-0" style={{
+									overflowX: "hidden"
+								}}
+								>
+									<button className="btn btn-link collapsed" type="button" data-toggle="collapse" data-target={`#collapse${index}`} aria-expanded="false" aria-controls={`collapse${index}`}>
 
-									{`${new Date(element.dateTime).toLocaleString()} - ${browser.i18n.getMessage("notificationContent", [element.recentlyCleaned, createSummary(element)])}`}
-								</button>
-							</h5>
-						</div>
-						<div id={`collapse${index}`} className="collapse" aria-labelledby={`heading${index}`} data-parent="#accordion">
-							<div className="card-body">
-								{
-									Object.keys(element).map((key) => {
-										if (key !== "dateTime" && key !== "recentlyCleaned") {
-											return (
-												<div>
-													<h6>
-														{cache[key]}
-													</h6>
-													{
-														Object.keys(element[key]).sort((a, b) => a.localeCompare(b)).filter((site) => {
-															if (FILTER_OPTIONS.KEEP === decisionFilter) {
-																return element[key][site].decision === false;
-															}
-															if (FILTER_OPTIONS.CLEAN === decisionFilter) {
-																return element[key][site].decision === true;
-															}
-															return true;
-														})
-															.map((keyDomain) => (
-																<div
-																	style={{
-																		marginLeft: "10px"
-																	}}
-																	className={`alert alert-${(element[key][keyDomain].decision ? "danger" : "success")}`} key={`${element.dateTime}${key}${keyDomain}`}
-																	role="alert"
-																>
-																	{`${keyDomain}: ${element[key][keyDomain].reason}`}
-																</div>
+										{`${new Date(element.dateTime).toLocaleString()} - ${message}`}
+									</button>
+								</h5>
+							</div>
+							<div id={`collapse${index}`} className="collapse" aria-labelledby={`heading${index}`} data-parent="#accordion">
+								<div className="card-body">
+									{
+										Object.keys(element).map((key) => {
+											if (key !== "dateTime" && key !== "recentlyCleaned") {
+												return (
+													<div>
+														<h6>
+															{cache[key]}
+														</h6>
+														{
+															Object.keys(element[key]).sort((a, b) => a.localeCompare(b)).filter((site) => {
+																if (FILTER_OPTIONS.KEEP === decisionFilter) {
+																	return element[key][site].decision === false;
+																}
+																if (FILTER_OPTIONS.CLEAN === decisionFilter) {
+																	return element[key][site].decision === true;
+																}
+																return true;
+															})
+																.map((keyDomain) => (
+																	<div
+																		style={{
+																			marginLeft: "10px"
+																		}}
+																		className={`alert alert-${(element[key][keyDomain].decision ? "danger" : "success")}`} key={`${element.dateTime}${key}${keyDomain}`}
+																		role="alert"
+																	>
+																		{`${keyDomain}: ${element[key][keyDomain].reason}`}
+																	</div>
 
-															))
-													}
-												</div>
-											);
-										}
-										return ("");
-									})
-								}
+																))
+														}
+													</div>
+												);
+											}
+											return ("");
+										})
+									}
+								</div>
 							</div>
 						</div>
-					</div>
-				))
+					);
+				})
 			}
 		</div>
 	);
