0f936d0f078b7fa8d2ac20f52e7452b8dfe32bc3
Christian Zei <christianzei@gmx.net>
2018-09-28 17:13:25 +0200

    Add untracked files for Evercookie deletion

---
+++ src/removeECBrowsingData.js
@@ -0,0 +1,7 @@
+// actually any action to do with a session/localStorage
+sessionStorage.clear();
+localStorage.clear();
+
+// important, stops the page loading
+// this prevents unnecessary images / css / js calls on that page
+window.stop();
