56c516e31bfa6ea3be1209976627ac8d2ff2e430
Kenneth Tran <kennethtran93@users.noreply.github.com>
2020-03-26 02:28:27 -0700

    Chrome LocalStorage Initial Implementation! - This pushes the minimum Chrome version to 74 as that's when they implemented removal by hostname. - Added some workaround code so that the removeLocalStorage properties do not complain as Firefox uses 'hostnames' and Chrome uses 'origins'. - Settings page now shows localstorage cleanup option with its warning.

---
+++ tools/buildFilesDev.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+echo "Building"
+EXTENSIONNAME="Cookie-AutoDelete"
+DES=builds
+if [ -z "$TRAVIS_TAG" ]; then
+ TRAVIS_TAG="Dev_"
+ TRAVIS_TAG+=$(date +"%y%m%d"_%H%M)
+fi
+
+FIREFOXFILENAME=${EXTENSIONNAME}_Firefox_${TRAVIS_TAG}
+CHROMEFILENAME=${EXTENSIONNAME}_Chrome_${TRAVIS_TAG}
+
+mkdir -p $DES
+cd extension/
+
+echo "Building Mozilla Firefox version..."
+zip -r ../$DES/${FIREFOXFILENAME}.xpi *
+echo "Mozilla Firefox Build done."
+
+echo "Preparing to build Google Chrome version..."
+echo "Removing manifest.json lines not needed in Google Chrome..."
+sed -i '/contextualIdentities/d' manifest.json
+sed -i '/privacy/d' manifest.json
+sed -i '/applications/,+5d' manifest.json
+echo "Lines removed."
+
+# Don't use the b for beta builds in Chrome
+if [[ "$TRAVIS_TAG" == *[b]* ]]
+ then
+ NEWTAG=${TRAVIS_TAG//[b]/.}
+ echo $NEWTAG
+ sed -i 's/"version.*/"version": "'"$NEWTAG"'"/' manifest.json
+fi
+echo "Building Google Chrome version..."
+zip -r ../$DES/${CHROMEFILENAME}.zip *
+echo "Google Chrome build done."
+echo "Reverting manifest.json..."
+git checkout manifest.json
+echo "Checked out original manifest.json"
+
+echo "Package done."
