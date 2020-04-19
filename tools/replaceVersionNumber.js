0d7cb04fa5cb762bb94fb0fe162428004a714f16
Kenneth Tran <kennethtran93@users.noreply.github.com>
2020-04-18 17:58:46 -0700

    tag 10

--- tools/replaceVersionNumber.js
+++ tools/replaceVersionNumber.js
@@ -28,8 +28,8 @@ if (process.env.TRAVIS_TAG !== undefined || process.env.GITHUB_REF !== undefined
   }
   console.log('New Version Number: ', versionTag);
   console.log('Replacing...');
-  changeVersion('package.json', versionTag);
-  changeVersion('extension/manifest.json', versionTag);
+  changeVersion('./package.json', versionTag);
+  changeVersion('./extension/manifest.json', versionTag);
   console.log('Replacements done with ' + versionTag);
 } else {
   console.log("CI TAG does not exist.  No Replacements done.");
