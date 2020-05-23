681a2b2ac2d2aec23a04bd10db3f630e8ddde1db
Kenneth Tran <kennethtran93@users.noreply.github.com>
2020-05-22 20:54:00 -0700

    minor update to tab query for global icon set.
    Signed-off-by: Kenneth Tran <kennethtran93@users.noreply.github.com>

--- src/services/BrowserActionService.ts
+++ src/services/BrowserActionService.ts
@@ -98,7 +98,7 @@ export const setGlobalIcon = async (enabled: boolean) => {
     });
 
     const tabAwait = await browser.tabs.query({
-      active: true,
+      active: enabled ? true : undefined,
       windowType: 'normal',
     });
     tabAwait.forEach(tab => {
