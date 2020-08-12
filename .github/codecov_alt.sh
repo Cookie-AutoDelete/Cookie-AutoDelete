2b6caeb355f34ba46bc7c8dc21c8cad6cb1b8038
Kenneth Tran <kennethtran93@users.noreply.github.com>
2020-08-12 16:02:35 -0700

    Test:  CodeCov Alternate
    Signed-off-by: Kenneth Tran <kennethtran93@users.noreply.github.com>

--- .github/codecov_alt.sh
+++ .github/codecov_alt.sh
@@ -5,8 +5,9 @@ EVENT_PAYLOAD_FILE=$1
 echo `jq "." $EVENT_PAYLOAD_FILE`
 PRID=`jq ".number" $EVENT_PAYLOAD_FILE`
 
-if [ $PRID = "null"]; then
+if [ $PRID = "null" ]; then
   bash <(curl -s https://codecov.io/bash) -f ./coverage/* -n  -F -Z
 else
+  echo "Pull Request Number Override:  $PRID"
   bash <(curl -s https://codecov.io/bash) -f ./coverage/* -P $PRID -n  -F -Z
 fi
