1ee14608baaa071a2da61423fe39195e5e0f62ae
Kenneth T <6724477+kennethtran93@users.noreply.github.com>
2022-04-30 17:48:50 -0700

    fix commit issues.
    Signed-off-by: Kenneth T <6724477+kennethtran93@users.noreply.github.com>

--- src/background.ts
+++ src/background.ts
@@ -233,11 +233,7 @@ browser.runtime.onStartup.addListener(async () => {
       } else {
         cadLog(
           {
-<<<<<<< HEAD
-            msg: 'Found a tab with [ about:sessionrestore ] in Firefox. Skipping Grey startup cleanup this time.',
-=======
             msg: 'Found a tab with [ about:sessionrestore ] in Firefox. Skipping Restart cleanup this time.',
->>>>>>> b5a8495 (Clarifying Language Usage.)
             type: 'info',
           },
           getSetting(store.getState(), SettingID.DEBUG_MODE) === true,
@@ -246,11 +242,7 @@ browser.runtime.onStartup.addListener(async () => {
     } else {
       cadLog(
         {
-<<<<<<< HEAD
-          msg: 'GreyList Cleanup setting is disabled.  Not cleaning cookies on startup.',
-=======
           msg: 'RestartList Cleanup setting is disabled.  Not cleaning cookies on startup.',
->>>>>>> b5a8495 (Clarifying Language Usage.)
           type: 'info',
         },
         getSetting(store.getState(), SettingID.DEBUG_MODE) === true,
