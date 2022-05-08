cfc3c82088ff88f0e9a0cc5b71f31a4bb403d296
Kenneth T <6724477+kennethtran93@users.noreply.github.com>
2022-05-07 19:39:33 -0700

    Refresh get-build PR command
    Signed-off-by: Kenneth T <6724477+kennethtran93@users.noreply.github.com>

--- .github/workflows/codecov_alt.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "$PR_NUM"
-if [ -z "$PR_NUM" ]; then
-  bash <(curl -s https://codecov.io/bash) -f ./coverage/* -Z
-  exit $?
-else
-  bash <(curl -s https://codecov.io/bash) -f ./coverage/* -P $PR_NUM -Z
-  exit $?
-fi
